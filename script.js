2eeb3ea9837d885eea85320bc70d5c0a78a4b728
Corey Sawin <sawincp@gmail.com>
2022-11-18 18:10:40 -0500

    update body toggle

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-const h2 = document.createElement("h2");
-h2.textContent = "What is Platform Tennis?";
-document.querySelector("body").appendChild(h2);
-
 function addingEventListener() {
     const about = document.getElementById('about');
     const stat = document.getElementById('stat');
