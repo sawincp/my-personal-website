d3ef7793b7f330293ccab52eb28de4a42fb9d0fe
Corey Sawin <sawincp@gmail.com>
2022-11-18 17:39:47 -0500

    Update to Nav Bar

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 const h2 = document.createElement("h2");
 h2.textContent = "What is Platform Tennis?";
 document.querySelector("body").appendChild(h2);
+
+function addingEventListener() {
+    const about = document.getElementById('about');
+    const stat = document.getElementById('stat');
+    const vid = document.getElementById('vid');
+
+function clickAlertAboutMe() {
+  alert('Player Page Loads');
+}
+function clickAlertStat(){
+    alert('Player Stats Load');
+}
+function clickAlertVid(){
+    alert('Video Loads');
+}
+
+about.addEventListener('click', clickAlertAboutMe);
+stat.addEventListener('click', clickAlertStat );
+vid.addEventListener('click', clickAlertVid);
+}
+addingEventListener();
